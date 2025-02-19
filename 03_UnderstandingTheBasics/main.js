ee7713db926d879a21a3bef7c84d4b6e3d3fae75
Tamana543 <tamanafarzami33@gmail.com>
2025-02-19 08:08:22 +0430

    New Dependenci Added

--- 03_UnderstandingTheBasics/main.js
+++ 03_UnderstandingTheBasics/main.js
@@ -1,12 +1,15 @@
 // const fs = require('fs')
 
 // const fs = require('fs')
-const root = require("./root.js")
+// const root = require("./root.js")
 const time = require("./date.js")
 // fs.writeFileSync('Test.html', "A File Made by React")
 // To creat a server by node.js
 const http = require('http') // in here you called the http path from node js and import it 
-const serverNew = http.createServer(root) // remember it need to be stored in a variable so that you can listen  it then. // it takes a function with 2 parametes (reqest and response)
+const express = require("express")
+// const serverNew = http.createServer(root) // remember it need to be stored in a variable so that you can listen  it then. // it takes a function with 2 parametes (reqest and response)
+const app = express()
+const serverNew = http.createServer(app)
 console.log(time);
 
 serverNew.listen(5430) // you need to add a path to the listener, and if you do not add it, it will get something automatically be itself
