aefbf2cdec9d001394795a289eb84dbca05dbad4
Tamana543 <tamanafarzami33@gmail.com>
2025-02-19 09:40:55 +0430

    body-parser package added

--- 03_UnderstandingTheBasics/main.js
+++ 03_UnderstandingTheBasics/main.js
@@ -1,21 +1,24 @@
-
 // const time = require("./date.js")
-// doing the app.js stafs with easily express
-
-const http = require('http') 
+// doing the app.js stafs (creating Server) with easily express
 const express = require("express")
-// const serverNew = http.createServer(root) 
+const bodyParser = require("body-parser")
 const app = express()
-app.use((req, res, next)=> {
-     console.log("Hello from Midleware First");
-     next() // call this so it can go through the rest of code, if we dont have res.Otherwise the req will die 
+
+// I need to parse the body of product to get a JSON file with key & Value
+app.use(bodyParser.urlencoded({extended : false}))
+// const serverNew = http.createServer(root) 
+app.use("/app-product" , (req, res, next)=> {
+     res.send("<form action='/product' method='POST' ><input type = 'text' name ='title' /> <button type ='submit'>Add To Shop </button></form>")
+     // next() // call this so it can go through the rest of code, if we dont have res.Otherwise the req will die 
 
 }) 
-app.use((req, res, next)=> {
-     // console.log("Hello form Midleware 2 ");
+app.use("/product", (req ,res ,next)=> {
+     console.log(req.body);
+     res.redirect('/')
+})
+app.use("/",(req, res, next)=> { // it takes one call back and a filter, Like above app if the url gith the path with / the page shows this server. And the other hund if it has a /app.. it will run above server
+     // console .log("Hello form Midleware 2 ");
      res.send('<h1> Tamana Farzami </h1>')
 }) 
-const serverNew = http.createServer(app)
+app.listen(5430) 
 // console.log(time);
-
-serverNew.listen(5430) 
