842a3eadc836120cea7ef503647c4033e904425a
Tamana543 <tamanafarzami33@gmail.com>
2025-03-06 09:59:23 +0430

    Server created: simple server created"

---
+++ Assignment/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app  = express()
+app.get('/',(req,res,nest)=>{
+res.render()
+})
+app.get('/users',(req,res,next)=>{
+     res.render()
+})
+app.post('/add-user',(req,res,next)=>{
+res.redirect('/users')
+})
+app.listen(5430)
