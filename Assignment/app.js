88fa716e7424b95fdcc7cc39f0bfa46285617ace
Tamana543 <tamanafarzami33@gmail.com>
2025-03-06 10:16:47 +0430

    Fo;der Updated

--- Assignment/app.js
+++ Assignment/app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app  = express()
+const users = [];
 app.set('view engine','ejs')
 app.set('views','views')
 app.get('/',(req,res,nest)=>{
-res.render()
+res.render('index',{pageTitle : 'Add User'})
 })
 app.get('/users',(req,res,next)=>{
-     res.render()
+     res.render('user',{pageTitle : "User"})
 })
 app.post('/add-user',(req,res,next)=>{
-res.redirect('/users')
+     users.push()
+     res.redirect('/users')
 })
+// 8:00
 app.listen(5430)
