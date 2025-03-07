62dfd2e0cf223e2c3f271d0d26f36c579cf992c5
Tamana543 <tamanafarzami33@gmail.com>
2025-03-07 09:26:52 +0430

    ejs Common start and end Layouts added

--- Assignment/app.js
+++ Assignment/app.js
@@ -4,14 +4,16 @@ const body = require('body-parser')
 const users = [];
 app.set('view engine','ejs')
 app.set('views','views')
+
+app.use(body.urlencoded({extended: false}))
 app.get('/',(req,res,nest)=>{
 res.render('index',{pageTitle : 'Add User'})
 })
 app.get('/users',(req,res,next)=>{
-     res.render('user',{pageTitle : "User"})
+     res.render('user',{pageTitle : "User",users : users})
 })
 app.post('/add-user',(req,res,next)=>{
-     users.push({userName : req.users.userName})
+     users.push({userName: req.body.userName})
      res.redirect('/users')
 })
 app.listen(5430)
