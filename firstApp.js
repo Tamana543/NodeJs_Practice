3619755da39f42262e0422ac099e6c98eeda9fb6
Tamana543 <tamanafarzami33@gmail.com>
2025-06-13 12:19:38 +0430

    practice 2 : First Sample server created

--- firstApp.js
+++ firstApp.js
@@ -2,5 +2,16 @@
 // const fs = require('fs')
 // fs.writeFileSync("text.txt","Hell I am new")
 const file = require('fs');
-const data = "I am from data";
-file.writeFileSync("Text.txt",data)
+// const data = "I am from data";
+// file.writeFileSync("Text.txt",data)
+
+//Server 
+const http = require('http')
+const server = http.createServer((req,res)=>{
+     // const data = String(req)
+     // file.writeFileSync("Text.txt",data)
+// console.log(res);
+// console.log("MEEEEEE");
+console.log(req);
+})
+server.listen(5430)
