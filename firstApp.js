4d9f3217aa0c6451d4abc83531d0d6612dd5748f
Tamana543 <tamanafarzami33@gmail.com>
2025-06-14 09:35:52 +0430

    Practice : 4 , request hundling

--- firstApp.js
+++ firstApp.js
@@ -10,14 +10,23 @@ const http = require('http')
 const server = http.createServer((req,res)=>{
      // const data = String(req)
      // file.writeFileSync("Text.txt",data)
-console.log(req);
+// console.log(req);
 // console.log("MEEEEEE");
-console.log(req.url,req.method,req.headers);
+// console.log(req.url,req.method,req.headers);
+const url = req.url
+if(url === "/") {
+ res.setHeader('content-type','text/Html')
+res.write('<html>')
+res.write('<head><title> My first Page of nodejs</title></head>')
+res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Submit</button></form></body> </li></ul>')
+res.write('</html>')
+return res.end()  // we return this from function to avoid excution of the next codes if the url is /   
+}
 // process.exit() // to show that the process sstop excution and exit 
 res.setHeader('content-type','text/Html')// to create the header for the page and the first parameter is saging .. the 2nd para tell you what type it should be
 res.write('<html>')// started wrtitin main html structure
 res.write('<head><title> My first Page of nodejs</title></head>')
-res.write('<body><h2>Hello From NodeJs darling</h2><ul> <li> <a href="https://www.nationalgeographic.com/animals/mammals/facts/domestic-cat">Press</a></body> </li></ul>')
+res.write(`<body><h2>Hello From NodeJs</h2><ul> <li> <a href="https://www.nationalgeographic.com/animals/mammals/facts/domestic-cat">Press</a></body> </li></ul>`)
 res.write('</html>')
 res.end()// to teel the respond it is ed of the project 
 })
