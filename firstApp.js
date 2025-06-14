26fcc734d4f319a557a83b80c13619d5a6136df4
Tamana543 <tamanafarzami33@gmail.com>
2025-06-14 09:03:46 +0430

    practice 4 : server respond and some req methods

--- firstApp.js
+++ firstApp.js
@@ -10,9 +10,15 @@ const http = require('http')
 const server = http.createServer((req,res)=>{
      // const data = String(req)
      // file.writeFileSync("Text.txt",data)
-// console.log(res);
-// console.log("MEEEEEE");
 console.log(req);
+// console.log("MEEEEEE");
+console.log(req.url,req.method,req.headers);
 // process.exit() // to show that the process sstop excution and exit 
+res.setHeader('content-type','text/Html')// to create the header for the page and the first parameter is saging .. the 2nd para tell you what type it should be
+res.write('<html>')// started wrtitin main html structure
+res.write('<head><title> My first Page of nodejs</title></head>')
+res.write('<body><h2>Hello From NodeJs darling</h2><ul> <li> <a href="https://www.nationalgeographic.com/animals/mammals/facts/domestic-cat">Press</a></body> </li></ul>')
+res.write('</html>')
+res.end()// to teel the respond it is ed of the project 
 })
 server.listen(5430)
