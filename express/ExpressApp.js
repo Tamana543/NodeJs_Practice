4e94ec77f7be3ef1e020b0f5381973e6749f469d
Tamana543 <tamanafarzami33@gmail.com>
2025-06-18 08:43:30 +0430

    expressApp updated: Crucial setups

--- express/ExpressApp.js
+++ express/ExpressApp.js
@@ -1,2 +1,11 @@
 const http = require('http')
 
+const express = require('express')
+
+const app = express() // Create an Express application (it is a function call as the express module exports a function)
+
+const server = http.createServer(app)// Create an HTTP server using the Express application
+
+server.listen(5430,()=>{
+     console.log("Hello from Express.js");
+})
