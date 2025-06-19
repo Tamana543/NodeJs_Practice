e23a7c9d66f8400d956011cdace98433a6751d70
Tamana543 <tamanafarzami33@gmail.com>
2025-06-19 10:35:30 +0430

    File Updated : the path are getting specefic

--- express/ExpressApp.js
+++ express/ExpressApp.js
@@ -7,20 +7,18 @@ const app = express() // Create an Express application (it is a function call as
 
 //keep in mind that the order of the middleware is important, the first one will run first and then the next one, so if you want to run a middleware after another one, you have to put it after the first one
 app.use(bodyParser.urlencoded({extended: false})) // Use body-parser middleware to parse URL-encoded bodies (like form submissions) and make the data available in req.body
-app.use('/add-product',(req,res)=>{
+app.use('/add-product',(req,res,next)=>{
      res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Enter product title" /><button type="submit">Add Product</button></form>')
+     next()
 })
 
 
-app.use('/product',(req,res)=>{
+app.post('/product',(req,res)=>{
      console.log(req.body);
-     res.redirect('/message') // Redirect the user to the /message route after processing the form submission
+     res.send('<h1>Product Added</h1>') // Send a response to the client
+     res.redirect('/') // Redirect the user to the  / route after processing the form submission
 })
 
-
-app.use('/message',(req,res,next)=>{
-     res.send("<h1>Hello From message</h1>") // Send a response to the client it is the end of the middleware chain, it sends the response and ends the request . It does the all res setHeader,write, and res.end in one go.
-})
 
 app.use('/',(req,res)=>{
      res.send('<h1>Hello From Express</h1>') // Send a response to the client
