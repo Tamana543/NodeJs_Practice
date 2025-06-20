a1adb7c180cea0b0c605f707d4cf125fd408baf8
Tamana543 <tamanafarzami33@gmail.com>
2025-06-20 12:59:44 +0430

    Path added

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,10 +1,12 @@
+const path = require('path')
 const express = require('express')
 
 const router = express.Router() // Create a new router 
 // Router is a mini express application that can be used to handle routes and middleware
 
 router.get('/add-product',(req,res,next)=>{
-     res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" placeholder="Enter product title" /><button type="submit">Add Product</button></form>')
+     // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" placeholder="Enter product title" /><button type="submit">Add Product</button></form>')
+     res.sendFile(path.join(__dirname,'../','Views','add-product.html'))
      // next()
 })
 
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,9 +1,10 @@
+const path = require('path') // Import the path module to work with file and directory paths (if not use this the node js will point to the all your pc ads driveC)
 const express= require('express');
 
 const router = express.Router()
 
 router.get('/',(req,res)=>{
-     res.send('<h1>Welcome to my online shop</h1>') // Send a response to the client
-})
+     res.sendFile(path.join(__dirname,'../','Views','shop.html'))// it will send this file as a respond so that it will bw shown in page. a : __dirname mean chick the folder that I write this code on its file  b : '../' as the dir point on main folder and we need to go up , c: Views the secpnd port in URL , d: the last port of the url and the file
+     })
 
 module.exports = router;
