f970f1eee052011aacb152a7257799e2f5c1a2b4
Tamana543 <tamanafarzami33@gmail.com>
2025-06-24 15:07:51 +0430

    Adding product pug file updated

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -5,9 +5,10 @@ const rootDir = require('../util/paths')
 const router = express.Router() // Create a new router 
 // Router is a mini express application that can be used to handle routes and middleware
 router.get('/add-product',(req,res,next)=>{
+     res.render('add-product',{pageTitle : "Add Product"})
      // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" placeholder="Enter product title" /><button type="submit">Add Product</button></form>')
      // res.sendFile(path.join(__dirname,'../','Views','add-product.html'))// there is a probem with it, it do not work for Linux or ther Os , as they start with \ not / so
-res.sendFile(path.join(rootDir,'Views','add-product.html')) // Use the rootDir utility to get the correct path to the Views directory
+// res.sendFile(path.join(rootDir,'Views','add-product.html')) // Use the rootDir utility to get the correct path to the Views directory
      // next()
 })
 
