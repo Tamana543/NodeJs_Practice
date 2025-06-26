e2a4f529b96b2eed3ea13cbd01cc76c9b3819f2c
Tamana543 <tamanafarzami33@gmail.com>
2025-06-26 09:25:23 +0430

    hbsScriptHandler in admin and shop added

--- express/routes/shop.js
+++ express/routes/shop.js
@@ -7,7 +7,7 @@ const router = express.Router()
 
 router.get('/',(req,res)=>{
      const products = adminData.product;
-     res.render('shop',{prods : products , pageTitle : 'Shop',path:'/',prodsExist : products.length > 0})// to render the default templates of shop.pug based the default tecmplate, and sinding the dynamic data (I had store and make it map through the js object ) to the pug file 
+     res.render('shop',{prods : products , pageTitle : 'Shop',path:'/',prodsExist : products.length > 0, activeShop: true,productCss : true})// to render the default templates of shop.pug based the default tecmplate, and sinding the dynamic data (I had store and make it map through the js object ) to the pug file 
      // res.sendFile(path.join(rootDir,'Views','shop.html'))// it will send this file as a respond so that it will bw shown in page. a : __dirname mean chick the folder that I write this code on its file  b : '../' as the dir point on main folder and we need to go up , c: Views the secpnd port in URL , d: the last port of the url and the file
      })
 
