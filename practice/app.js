3a44ca863a48b44ad996ccec7e9da9d3bcb2ce9f
Tamana543 <tamanafarzami33@gmail.com>
2025-06-28 09:00:29 +0430

    folder updated

--- practice/app.js
+++ practice/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const bodyPars = require('body-parser')
 
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
+app.use(bodyPars.urlencoded({extended:false}))
 
+const users = []
 app.get('/',(req,res,next)=>{
     res.render('index',{pageTitle : 'Admins'}) 
 })
@@ -12,6 +15,7 @@ app.get('/users',(req,res,next)=>{
    res.render('admin',{pageTitle: 'Add admin'}) 
 })
 app.post('/add-user',(req,res,next)=>{
+     users.push()
  res.redirect('/') 
 })
 app.listen(3000)
