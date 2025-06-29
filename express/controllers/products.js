f1d30ee651ec7df03e275d1a44da0a8a5c51a13f
Tamana543 <tamanafarzami33@gmail.com>
2025-06-29 16:15:10 +0430

    Project updated: products functionality transfered to controller

--- express/controllers/products.js
+++ express/controllers/products.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path') // Import the path module to work with file and directory paths (if not use this the node js will point to the all your pc ads driveC)
 const rootDir = require('../util/paths') 
 
 const product = [];
@@ -17,3 +17,15 @@ exports.postAddProduct = (req,res)=>{
      product.push({title : req.body.title})
      res.redirect('/') // Redirect the user to the  / route after processing the form submission
 }
+
+exports.postShopPage = (req,res)=>{
+ 
+     // console.log(products);
+     res.render('shop',{prods : product ,
+           pageTitle : 'Shop',
+          path: '/',
+          prodsExist : product.length > 0,
+           activeShop: true,
+          productCss : true})// to render the default templates of shop.pug based the default tecmplate, and sinding the dynamic data (I had store and make it map through the js object ) to the pug file 
+     // res.sendFile(path.join(rootDir,'Views','shop.html'))// it will send this file as a respond so that it will bw shown in page. a : __dirname mean chick the folder that I write this code on its file  b : '../' as the dir point on main folder and we need to go up , c: Views the secpnd port in URL , d: the last port of the url and the file
+     }
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,20 +1,10 @@
-const path = require('path') // Import the path module to work with file and directory paths (if not use this the node js will point to the all your pc ads driveC)
+const productController = require('../controllers/products')
 
 const express= require('express');
-const rootDir = require('../util/paths') 
-const adminData = require('./admin')
+
+
 const router = express.Router()
 
-router.get('/',(req,res)=>{
-     const products = adminData.product;
-     // console.log(products);
-     res.render('shop',{prods : products ,
-           pageTitle : 'Shop',
-          path: '/',
-          prodsExist : products.length > 0,
-           activeShop: true,
-          productCss : true})// to render the default templates of shop.pug based the default tecmplate, and sinding the dynamic data (I had store and make it map through the js object ) to the pug file 
-     // res.sendFile(path.join(rootDir,'Views','shop.html'))// it will send this file as a respond so that it will bw shown in page. a : __dirname mean chick the folder that I write this code on its file  b : '../' as the dir point on main folder and we need to go up , c: Views the secpnd port in URL , d: the last port of the url and the file
-     })
+router.get('/',productController.postShopPage)
 
 module.exports = router;
