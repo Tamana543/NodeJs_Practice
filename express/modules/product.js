ee5c00df16001842d05ac5800c7996d8b92a76c3
Tamana543 <tamanafarzami33@gmail.com>
2025-07-02 15:30:39 +0430

    File Debugged

--- express/modules/product.js
+++ express/modules/product.js
@@ -1,52 +1,39 @@
-const { fileLoader } = require('ejs')
+
 const fs = require('fs')
 const path = require('path')
 
- const p = path.join(
+const p = path.join(
           path.dirname(process.mainModule.filename),
           'data',
           'products.json'
         )
-        const fetchDataFromFile = (cb,err)=>{
-           if (err) {
-               return cb([]);
+const fetchDataFromFile = cb=>{
+     fs.readFile(p,(err,fileContent)=>{
+
+          if (err) {
+              return cb([]);
           }
-          try {
-               
-               cb(JSON.parse(fileContent));
-               cb(products)
-          } catch (error) {
-               console.log("error from fetch:",error);
-               cb([])
-          }
-        }
+          cb(JSON.parse(fileContent));
+    
+     })
+}
 module.exports = class Product{
      constructor(t){
           this.title = t
      }
 
      save(){
-         
-          fs.readFile( p, (err, fileContent)=>{
-          let products = []
-          if(!err){
-               try {
-                    
-                    products = JSON.parse(fileContent)
-               } catch (error) {
-                    console.log('Error ', error);
-               }
-          }
+         fetchDataFromFile(products=>{
           products.push(this)
-          fs.writeFile(p, JSON.stringify(products), err => {
-                    console.log('Error writing files ',err);
-               });
-          });
+          fs.writeFile(p, JSON.stringify(products),err=>{
+               console.log(err);
+          })
+         })
+         
      }
      static fetchAll(cb) {
-       fs.readFile(p, (err, fileContent) => {
-         fetchDataFromFile(cb)
-        });
+          fetchDataFromFile(cb)
+ 
       }
-     }
+}
 
