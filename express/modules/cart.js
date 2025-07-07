e31950e5b7229398e849c5d322f03e36b1680b44
Tamana543 <tamanafarzami33@gmail.com>
2025-07-07 16:21:20 +0430

    file updated

--- express/modules/cart.js
+++ express/modules/cart.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
-const { json } = require('body-parser')
-const Product = require('./product')
+
+
 
 const p = path.join(
           path.dirname(process.mainModule.filename),
@@ -13,22 +13,22 @@ static addProduct(id,productPrice) {
      //lets fetch the previeus cart first 
 
 fs.readFile(p,(err,fileContent)=>{
-     let cart = {Product: [] , totalPrice : 0}
+     let cart = {product: [] , totalPrice : 0}
 if(!err) {
  cart = JSON.parse(fileContent)
 }
 // Analyse the cart 
-const existantCartIndex = cart.Product.findIndex(prod => prod.id === id) 
-const existantCart = cart.Product.find(existantCartIndex) 
+const existantProductIndex = cart.product.findIndex(prod => prod.id === id) 
+const existantProduct = cart.product[existantProductIndex]
 let updatedProduct;
-if(existantCart){
-updatedProduct = [...existantCart]
-updatedProduct.qty = updatedProduct.qty+1
-cart.Product= [...cart.Product]
-cart.Product[existantCartIndex] = updatedProduct
+if(existantProduct){
+updatedProduct = [...existantProduct]
+updatedProduct.qty = updatedProduct.qty + 1
+cart.product= [...cart.product]
+cart.product[existantProductIndex] = updatedProduct
 }else {
      updatedProduct = {id : id , qty : 1}
-     cart.Product = [...cart.Product,updatedProduct]
+     cart.product = [...cart.product,updatedProduct]
 }
 cart.totalPrice = cart.totalPrice + +productPrice
 fs.writeFile(p, JSON.stringify(cart) ,err => {
