6bc6a172c978134b3407c63a0d9aa9e30a72ed13
Tamana543 <tamanafarzami33@gmail.com>
2025-07-26 10:23:58 +0430

    Project switched to MangoDB

--- express/ExpressApp.js
+++ express/ExpressApp.js
@@ -4,17 +4,9 @@ const bodyParser = require('body-parser') // Import body-parser to parse incomin
 const app = express() // Create an Express application (it is a function call as the express module exports a function)
 // const expressHbs = require('express-handlebars');
 const errorController = require('./controllers/404')
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
-const sequalizer = require('./util/database')
-const Product = require('./modules/product')
-const User = require('./modules/user')
-const Cart = require('./modules/cart')
-const cartItems = require('./modules/cartItem')
-const Order = require('./modules/order') ;
-const orderItems = require('./modules/orderItem') ;
-const OrderItem = require('./modules/orderItem')
-const { FORCE } = require('sequelize/lib/index-hints')
+// const adminRoutes = require('./routes/admin')
+// const shopRoutes = require('./routes/shop')
+
 // Wiew enjine hundlers 
 // app.engine('hbs', expressHbs({layoutsDir : 'views/layouts',defaultLayout : 'main-layout',extname : 'hbs'}))//it use with the templating language engines that are not inide express js itself and you need to tell it to run the file base the givin engine, the first para is the name(as your wish), the second one is the variable you had exported above (there is a bugg with it)
 app.set('view engine','ejs')// setting the default templating enginge to the handlebar file 
@@ -35,11 +27,11 @@ app.use(express.static(path.join(__dirname,'public')))// use thi line so that yo
 // sequalizer userTable middleware
 
 app.use((req,res,next) =>{
-User.findByPk(1).then(user=>{
-     console.log(user);
-     req.user = user;
-     next()
-}).catch(err=>console.log(err))
+// User.findByPk(1).then(user=>{
+//      console.log(user);
+//      req.user = user;
+//      next()
+// }).catch(err=>console.log(err))
 })
 app.use('/admin',adminRoutes)
 
@@ -48,37 +40,50 @@ app.use(shopRoutes)
 app.use(errorController.get404)
 // const server = http.createServer(app)// Create an HTTP server using the Express application
 // server.listen(5430) or
-
+//Sequalizer
+/** 
+ const sequalizer = require('./util/database')
+const Product = require('./modules/product')
+const User = require('./modules/user')
+const Cart = require('./modules/cart')
+const cartItems = require('./modules/cartItem')
+const Order = require('./modules/order') ;
+const orderItems = require('./modules/orderItem') ;
+const OrderItem = require('./modules/orderItem')
+const { FORCE } = require('sequelize/lib/index-hints')
 // lets work with sequalizer and let it to run  the server 
-// Association ( search in sequalizer Doc)
-// I will keep the user s the connector between all moculews and tables: as the user is the one with many products one card and even order so 
-Product.belongsTo(User,{constraints : true, onDelete: 'CASCADE'})// onDelete CASCADE means if I deleted the user delete its products too
-User.hasMany(Product)
-User.hasOne(Cart)
-Cart.belongsTo(User)
-User.hasOne(cartItems)
-Cart.belongsToMany(Product , {through : cartItems})
-Product.belongsToMany(Cart , {through : cartItems})
-Order.belongsTo(User)
-User.hasMany(Order)
-Order.belongsToMany(Product,{through : OrderItem})
+     // Association ( search in sequalizer Doc)
+     // I will keep the user s the connector between all moculews and tables: as the user is the one with many products one card and even order so 
+     Product.belongsTo(User,{constraints : true, onDelete: 'CASCADE'})// onDelete CASCADE means if I deleted the user delete its products too
+     User.hasMany(Product)
+     User.hasOne(Cart)
+     Cart.belongsTo(User)
+     User.hasOne(cartItems)
+     Cart.belongsToMany(Product , {through : cartItems})
+     Product.belongsToMany(Cart , {through : cartItems})
+     Order.belongsTo(User)
+     User.hasMany(Order)
+     Order.belongsToMany(Product,{through : OrderItem})
 
-sequalizer.sync({FORCE : true}).then(result=>{
-     // force is use in parameter  ({force : true}) here to make a database everytime we wont to overwrote (dependesy : delete at the end)
-     return User.findByPk(1)
-     // console.log(result);
-}).then(user=>{
-     if(!user){
-          return User.create({name : 'Tamana',email : 'tamana.farzami33@gmail.com'})
-     }
-     return user;
-}).then((user)=>{
+     sequalizer.sync({FORCE : true}).then(result=>{
+          // force is use in parameter  ({force : true}) here to make a database everytime we wont to overwrote (dependesy : delete at the end)
+          return User.findByPk(1)
+          // console.log(result);
+     }).then(user=>{
+          if(!user){
+               return User.create({name : 'Tamana',email : 'tamana.farzami33@gmail.com'})
+          }
+          return user;
+     }).then((user)=>{
+          
+          return user.createCart();
+          // console.log(user);
      
-     return user.createCart();
-     // console.log(user);
-  
-}).then(user=>{
-app.listen(5430)
-}).catch(err=>{
-     console.log(err);
-})
+     }).then(user=>{
+     app.listen(5430)
+     }).catch(err=>{
+          console.log(err);
+     })
+*/
+
+// MANGO DB
