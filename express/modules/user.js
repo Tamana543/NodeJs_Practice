edbb2eb463b1ca5eefc10349e97a1e0df2866640
Tamana543 <tamanafarzami33@gmail.com>
2025-08-01 14:40:50 +0430

    creating a user by mongo db :..

--- express/modules/user.js
+++ express/modules/user.js
@@ -1,17 +1,32 @@
-const Sequalizer = require('sequelize')
-const sequalizer = require('../util/database')
+// mongoDB
 
-const User = sequalizer.define('userProduct',{
-id : {
- type:Sequalizer.INTEGER,
-          autoIncrement : true,
-          allowNull : false,
-          primaryKey : true,
-},
-name: Sequalizer.STRING,
-email  :{
-          type : Sequalizer.STRING,
-          allowNull : false,
+const { name } = require("ejs");
+const mongoDB = require("mongodb")
+const getDb = require('../util/database').getDb;
+//with Sequalizeer
+class User {
+     constructor(username, email){
+this.username= name,
+this.email=email
      }
-})
+
+}
+/**
+ // const Sequalizer = require('sequelize')
+// const sequalizer = require('../util/database')
+
+// const User = sequalizer.define('userProduct',{
+// id : {
+//  type:Sequalizer.INTEGER,
+//           autoIncrement : true,
+//           allowNull : false,
+//           primaryKey : true,
+// },
+// name: Sequalizer.STRING,
+// email  :{
+//           type : Sequalizer.STRING,
+//           allowNull : false,
+//      }
+// })
+**/
 module.exports = User;
