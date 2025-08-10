ee67588ba5cf752ecc8fd8fd6b374ad4360236c2
Tamana543 <tamanafarzami33@gmail.com>
2025-08-10 14:49:52 +0430

    Order.js file started

---
+++ express/modules/order.js
@@ -0,0 +1,13 @@
+// const Sequalizer = require('sequelize');
+
+// const sequalize = require('../util/database')
+
+// const Order = sequalize.define('order',{
+//      id : {
+//              type: Sequalizer.INTEGER,
+//                allowNull : false,
+//                autoIncrement : true,
+//                primaryKey : true
+//      }
+// })
+// module.exports = Order;
