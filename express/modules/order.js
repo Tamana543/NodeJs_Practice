639b8620bb6c9915fc2158c64c7c95dde1653eef
Tamana543 <tamanafarzami33@gmail.com>
2025-08-11 16:47:11 +0430

    order.js file optimized

--- express/modules/order.js
+++ express/modules/order.js
@@ -27,16 +27,3 @@ user :{
 }
 })
 module.exports = mongoose.model('Order',OrderSchema)
-// const Sequalizer = require('sequelize');
-
-// const sequalize = require('../util/database')
-
-// const Order = sequalize.define('order',{
-//      id : {
-//              type: Sequalizer.INTEGER,
-//                allowNull : false,
-//                autoIncrement : true,
-//                primaryKey : true
-//      }
-// })
-// module.exports = Order;
