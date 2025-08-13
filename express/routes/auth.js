b1aad5c0e6808d5a4a99c70b3724fc9a362f5a02
Tamana543 <tamanafarzami33@gmail.com>
2025-08-13 18:39:28 +0430

    folder updated

--- express/routes/auth.js
+++ express/routes/auth.js
@@ -2,4 +2,6 @@ const authController =  require('../controllers/auth')
 const express = require('express')
 const router = express.Router()
 
+
+router.get('/',authController.getLogin)
 module.exports = router;
