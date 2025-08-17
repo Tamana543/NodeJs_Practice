9bfc239e2a8201ff15eadb511ef75af2ce60590b
Tamana543 <tamanafarzami33@gmail.com>
2025-08-17 17:15:50 +0430

    my first cookie added

--- express/controllers/auth.js
+++ express/controllers/auth.js
@@ -1,15 +1,18 @@
 exports.getLogin = (req,res)=>{
+  const isLoggedIn =req.get('Cookie').trim().split('=')[1] ;
+
 res.render('auth/login',{
     pageTitle : "Login Page",
       path : '/login',
-        isAuthCorrect : req.isLoggedIn
+        isAuthCorrect : isLoggedIn
    
   })
 
 
 }
 exports.postLogin = (req,res)=>{
-  req.isLoggedIn = true
+  // here you need to store users data so you can understand whose fie are these : cookie
+ res.setHeader('Set-Cookie',"isLoggedIn=true")
 res.redirect('/')
 }
 
