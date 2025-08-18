72241482ae8cc52d866e15ce7917f5b68eee5c44
Tamana543 <tamanafarzami33@gmail.com>
2025-08-18 16:04:08 +0430

    express session istalled

--- express/controllers/auth.js
+++ express/controllers/auth.js
@@ -1,10 +1,12 @@
 exports.getLogin = (req,res)=>{
-  const isLoggedIn =req.get('Cookie').trim().split('=')[1] ;
+ 
+  // const isLoggedIn =req.get('Cookie').trim().split('=')[1]==='true' ;
 
 res.render('auth/login',{
     pageTitle : "Login Page",
       path : '/login',
-        isAuthCorrect : isLoggedIn
+        // isAuthCorrect : isLoggedIn
+        isAuthCorrect : false
    
   })
 
@@ -12,7 +14,7 @@ res.render('auth/login',{
 }
 exports.postLogin = (req,res)=>{
   // here you need to store users data so you can understand whose fie are these : cookie
- res.setHeader('Set-Cookie',"isLoggedIn=true")
+ res.setHeader('Set-Cookie',"isLoggedIn=true; secure")
 res.redirect('/')
 }
 
